90576c4bff3fc3b4f451df51eef3665b953d9da4
jsanchezmkx <jsanchez@mkx.cl>
2020-05-13 16:53:43 -0400

    Update sms.py

--- src/sms.py
+++ src/sms.py
@@ -1,18 +1,20 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 from twilio.rest import Client
 
-def send(body='Some body', to=''):
+def send(body, to=''):
     # Your Account Sid and Auth Token from twilio.com/console
     # DANGER! This is insecure. See http://twil.io/secure
-    account_sid = ''
-    auth_token = ''
+    account_sid = 'AC9706b47d65d3d7da5f0ba9b0f521ced2'
+    auth_token = '291554e1cb43a6df5bcbe5812ce26dab'
     client = Client(account_sid, auth_token)
 
     message = client.messages \
                     .create(
                         body=body,
-                        from_='+13346058062',
-                        to='+'+to
+                        from_='+12058757677',
+                        to='+56951147400'
                     )
 
     print(message.sid)
+mensaje="hola mmgvo"
+send(mensaje)
